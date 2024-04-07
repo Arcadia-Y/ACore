e855852ea0b9e6f60c1b90d52d3b51c437a45dcd
Arcadia-Y <2271568957@qq.com>
2024-04-07 15:30:51 +0800

    feat(spin): implement spin lock

---
+++ spin/src/lib.rs
@@ -0,0 +1,54 @@
+// a simple spin lock
+// reference: https://www.oreilly.com/library/view/rust-atomics-and/9781098119430/ch04.html
+
+#![no_std]
+
+use core::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    data: UnsafeCell<T>,
+}
+
+impl<T> SpinLock<T> {
+    pub fn new(data: T) -> Self {
+        SpinLock {
+            locked: AtomicBool::new(false),
+            data: UnsafeCell::new(data),
+        }
+    }
+
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, Ordering::Acquire) {
+            // spin until the lock is acquired
+        }
+        Guard { lock: self }
+    }
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.data.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.data.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Ordering::Release);
+    }
+}
